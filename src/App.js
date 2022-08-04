042768e96e555516d47cd7b6d5fe7b38a127b094
aymenmzz <105977772+aymenmzz@users.noreply.github.com>
2022-08-04 08:56:43 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import youtubeApiSearch from "youtube-api-search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+const API_KEY = 'AIzaSyAnWIoN061xTd_Zuls_daXKAqBfRusYjaY';
+const [val, setVal] = React.useState([])
+// React.useEffect(()=> {
+//   youtubeApiSearch({key: API_KEY, term: "pbd"}, data => setVal([data[0]]))
+
+// }, [])
+
+const [valInput, setValInput] = React.useState('')
+
+const handleChange = (event ) =>{
+  setValInput(event.target.value)
 }
 
-export default App;
+const handleSubmit = (event) => {
+  event.preventDefault()
+  youtubeApiSearch({key: API_KEY, term: valInput}, data => setVal([data[0]]))
+  setValInput("")
+} 
+function html (text) {
+  const doc = document.createElement("h2")
+  doc.innerHTML = text
+  return doc.innerText
+}
+  return (
+    <>
+      <Header />
+      <form onSubmit={handleSubmit} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <input style={{padding: 10, margin: 15, border: "2px solid lightgray", borderRadius: "5px", width: "30%"}} type="text" value={valInput} onChange={handleChange} />
+      </form>
+    {val.map((v, index) => (<div key={index} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <h2 style={{textAlign: "center"}}>{html(v.snippet.title)}</h2>
+    <iframe style={{width: "60vw", height: "60vh"}} src= {`https://youtube.com/embed/${v.id.videoId}`} />
+    <br/>
+    </div>))}
+    <br/>
+    </>
+  );
+}
